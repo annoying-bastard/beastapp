2e999ab178a6dbb1fd415c78438029eb35e2b390
annoying bastard <annoying.bastard@gmx.de>
2017-04-10 12:57:35 +0200

    build ready beastapp

---
+++ index.js
@@ -0,0 +1,112 @@
+  console.log(window);
+  window.beasts = (() => {
+    var plan = [
+      '####################',
+      '#__________________#',
+      '#__________T_______#',
+      '#_______________o__#',
+      '#____o_____________#',
+      '#__________________#',
+      '#__________________#',
+      '#___________O______#',
+      '#__________________#',
+      '#____T_____________#',
+      '####################'
+    ]
+
+    const state = {
+      plan: plan.map((string) => string.split('')),
+      beasts: [],
+    };
+
+    const getVectorContent = (state, vector) => {
+      return plan[vector.y][vector.x];
+    }
+
+    const checkIfTraversable = (plan, vector) => {
+      if(vector && plan && plan[vector.y][vector.x] === '_') { return true; }
+      else { return false; }
+    }
+
+    class Beast {
+      constructor() {
+        this.x = Math.floor(Math.random() * 7);
+        this.y = Math.floor(Math.random() * 7);
+        this.move = this.move.bind(this);
+        this.newVector={};
+      }
+
+      placeBeast(plan) {
+        if (checkIfTraversable(state.plan, this)){
+          plan[this.y][this.x] = 'B';
+          state.beasts.push(this);
+        }
+      }
+
+      move() {
+        var direction = Math.floor(Math.random() * 4);
+        this.newVector.x = this.x;
+        this.newVector.y = this.y;
+
+        switch(direction){
+          case 0:
+            this.newVector.x++
+            break;
+          case 1:
+            this.newVector.y++;
+            break;
+          case 2:
+            this.newVector.x--;
+            break;
+          case 3:
+            this.newVector.y--;
+            break;
+          default:
+            break;
+        }
+
+        if (checkIfTraversable(state.plan, this.newVector)){
+          updatePlan(this, '_');
+          this.x = this.newVector.x;
+          this.y = this.newVector.y;
+          updatePlan(this, 'B');
+        }
+      }
+    }
+
+    const updatePlan = (vector, newTile) => {
+      state.plan[vector.y][vector.x] = newTile;
+    }
+
+    const render = (state) => {
+      let app = document.getElementById('app')
+      app.innerHTML = null;
+      state.plan.map((curr) => curr.join(''))
+        .map((curr) => {
+          let node = document.createElement('li')
+          node.innerText = curr
+          app.appendChild(node)
+        })
+    }
+
+    const tick = () => {
+      if (state.beasts.length < 4){
+        let beast = new Beast();
+        beast.placeBeast(state.plan);
+      }
+      state.beasts.map((curr) => curr.move());
+      render(state);
+    }
+
+    let run;
+
+    const start = (speed) => {
+        run = setInterval(tick, speed || 1000);
+    }
+
+    const stop = () => {
+      clearInterval(run);
+    }
+
+    return { start, stop }
+  })();
